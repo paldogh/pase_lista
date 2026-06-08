15c774055ae0bea55d84ffc71fac7f5f360baa00
MelanieSelAud <melanieselenaaudreygranados@gmail.com>
2026-06-08 15:25:43 -0600

    Agregar filtros de alumnos por grupo, matricula, nombre y faltas
    - Filtro por grupo
    - Filtro por matricula
    - Filtro por nombre
    - Filtro de alumnos con faltas

--- backend_qr/src/controllers/alumnos.controllers.js
+++ backend_qr/src/controllers/alumnos.controllers.js
@@ -25,3 +25,83 @@ message: "Alumnos obtenidos correctamente",
 
 }
 }
+
+/*Se agregan las rutas */
+
+// Buscar alumnos por grupo
+export const obtenerAlumnosPorGrupo = async (req, res) => {
+    try {
+        const { grupo } = req.params; //aqui agarra el grupo que se le pone a la URL
+        const alumnos = await prisma.alumno.findMany({
+            where: {
+                grupo: { nombre: grupo }
+            },
+            include: { grupo: true }
+        });
+        return res.status(200).json({ ok: true, data: alumnos });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, msg: "Error del servidor" });
+    }
+}
+
+// Buscar alumno por matrícula
+export const obtenerAlumnoPorMatricula = async (req, res) => {
+    try {
+        const { matricula } = req.params;//aqui agarra la maticula que se le pone en la URL
+        const alumno = await prisma.alumno.findUnique({
+            where: { matricula },
+            include: { grupo: true }
+        });
+        if (!alumno) return res.status(404).json({ ok: false, msg: "Alumno no encontrado" });
+        return res.status(200).json({ ok: true, data: alumno });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, msg: "Error del servidor" });
+    }
+}
+  
+// Buscar alumnos por nombre
+export const obtenerAlumnosPorNombre = async (req, res) => {
+    try {
+        const { nombre } = req.params; //aqui agarra el nombre que se le pone en la URL
+        const alumnos = await prisma.alumno.findMany({
+            where: {
+                nombre: {
+                    contains: nombre,
+                    mode: "insensitive"//busca alumnos que contengan ese nombre, sin importar si mayuscula o minuscula
+                }
+            },
+            include: { grupo: true }
+        });
+        return res.status(200).json({ ok: true, data: alumnos });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, msg: "Error del servidor" });
+    }
+}
+ 
+// Buscar alumnos con faltas
+export const obtenerAlumnosConFaltas = async (req, res) => {
+    try {
+        const alumnos = await prisma.alumno.findMany({
+            where: {
+                asistencias: {
+                    some: {
+                        estado: "FALTA"
+                    }
+                }
+            },
+            include: {
+                grupo: true,
+                asistencias: {
+                    where: { estado: "FALTA" }
+                }
+            }
+        });
+        return res.status(200).json({ ok: true, data: alumnos });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, msg: "Error del servidor" });
+    }
+}
--- backend_qr/src/routes/alumnos.routes.js
+++ backend_qr/src/routes/alumnos.routes.js
@@ -2,6 +2,24 @@
 
 import { Router } from 'express';
 import { obtenerAlumnos } from '../controllers/alumnos.controllers.js';
+/*----------------------Se importan las rutas nuevas--------------------------------------------------------------*/
+import { obtenerAlumnosPorGrupo } from '../controllers/alumnos.controllers.js'; //1ra ruta buscar alumnos por grupo
+import { obtenerAlumnoPorMatricula } from '../controllers/alumnos.controllers.js'; //2da ruta obtener alumnos por matricula
+import { obtenerAlumnosPorNombre } from '../controllers/alumnos.controllers.js'; //3ra ruta para obtener alumnos por nombre
+import { obtenerAlumnosConFaltas } from '../controllers/alumnos.controllers.js'; //4ta ruta para obtener alumnos con falta
 const router = Router();
 router.get('/', obtenerAlumnos);
+/*----------------------Rutas de la api--------------------------------*/
+// buscar alumnos por grupo
+router.get( '/grupo/:grupo', obtenerAlumnosPorGrupo );
+
+//buscar alumno por matricula
+router.get ('/matricula/:matricula', obtenerAlumnoPorMatricula );
+
+//buscar alumnos por nombre 
+router.get ('/buscar/:nombre', obtenerAlumnosPorNombre);
+
+//buscar alumnos con falta
+router.get ('/faltas', obtenerAlumnosConFaltas); 
+
 export default router;
